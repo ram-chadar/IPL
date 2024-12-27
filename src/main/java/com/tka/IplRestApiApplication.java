7c79a53c0bf610e55456c6d2d2e7c9f7a74ffde5
ram-chadar <javaprojectwork2715@gmail.com>
2024-12-27 18:25:17 +0530

    remove empty line

--- src/main/java/com/tka/IplRestApiApplication.java
+++ src/main/java/com/tka/IplRestApiApplication.java
@@ -1,5 +1,4 @@
 package com.tka;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ public class IplRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IplRestApiApplication.class, args);
-		System.out.println("app started");
+		System.out.println("app  started");
 	}
 
 }
